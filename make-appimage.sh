120207b16cc141118cd00c76a3114c3e39e4d61f
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-01-22 10:05:06 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,11 +8,11 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/512x512/apps/org.fedoraproject.MediaWriter.png
+export DESKTOP=/usr/share/applications/org.fedoraproject.MediaWriter.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/mediawriter /usr/lib/mediawriter/helper
 
 # Additional changes can be done in between here
 
